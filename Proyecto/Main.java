933556bfe6f51bb9e6051c0398202c6fbe5d6379
Juan-Felix11 <juanfelixcastillo06@gmail.com>
2025-05-03 10:26:24 -0600

    Se agregó el main, por el momento ssolo del ahorcado

---
+++ Proyecto/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // Crea una instancia el juego y lo inicia
+        JuegoAhorcado juego = new JuegoAhorcado();
+        juego.iniciarJuego();
+    }
+
+}
