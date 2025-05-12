a26b702f4ea1bb86e5889e8a234f0fa2b5a71302
keylorac <keylorac@outlook.es>
2025-05-12 02:18:51 -0600

    Main con menu

--- Proyecto/Main.java
+++ Proyecto/Main.java
@@ -16,11 +16,32 @@ public class Main {
             System.out.println("0. Salir");
             System.out.print("Seleccione una opción: ");
             opcion = scanner.nextInt();
-        }
 
-        // Crea una instancia el juego y lo inicia
-        JuegoAhorcado juego = new JuegoAhorcado();
-        juego.iniciarJuego();
+            //usamos estructuras de control vistas en clase para el menú
+            switch(opcion)
+            {
+                case 1:
+                    //Creamos una instancia del juego BatallaNaval y se inicializa
+                    BatallaNaval batallaNaval = new BatallaNaval();
+                    batallaNaval.iniciarJuego();
+                    break;
+
+                case 2:
+                    // Crea una instancia el juego y lo inicia
+                    JuegoAhorcado juego = new JuegoAhorcado();
+                    juego.iniciarJuego();
+                    break;
+
+                case 0:
+                    //Se sale del programa
+                    System.out.println("Gracias por jugar");
+                    break;
+                default:
+                    //Control de errores con un mensaje en caso de eerror
+                    System.out.println("Por favor, intente de nuevo.");
+            }
+        } while (opcion != 0);
+
     }
 
 }
